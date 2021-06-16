4c4e54fb97c2b7f99bc6864d29dc3ab7fc22be97
muhammed hosny <muhammed195772@feng.bu.edu.eg>
2021-06-16 10:22:44 +0200

    a

--- start over/script.js
+++ start over/script.js
@@ -9,6 +9,10 @@ let goToGalleryClasses = document.getElementsByClassName("go-to-gallery");
 for (let i = 0; i < goToGalleryClasses.length; i++) {
     goToGalleryClasses[i].addEventListener("click", function() { location.href = "./gallery/index.html" });
 }
+let goToFavourites = document.getElementsByClassName("go-to-favourites");
+for (let i = 0; i < goToFavourites.length; i++) {
+    goToFavourites[i].addEventListener("click", function() { location.href = "./favourites/index.html" });
+}
 let goToRegister = document.getElementsByClassName("go-to-register");
 for (let i = 0; i < goToRegister.length; i++) {
     goToRegister[i].addEventListener("click", function() { location.href = "./register/index.html" });
